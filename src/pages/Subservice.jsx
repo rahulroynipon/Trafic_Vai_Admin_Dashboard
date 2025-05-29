52c56720d0a6f665130bd08cc57c17209064648e
rahulroynipon <rahulroynipon@gmail.com>
2025-05-29 21:02:14 +0600

    feat: enhance Subservice and SubserviceList components; add conditional rendering for subservices and improve loading state handling

--- src/pages/Subservice.jsx
+++ src/pages/Subservice.jsx
@@ -6,16 +6,26 @@ import { useEffect } from "react";
 function Subservice() {
   const { slug } = useParams();
 
-  const { getSubServicesHandler } = useServiceStore();
+  const { getSubServicesHandler, subservices } = useServiceStore();
 
   useEffect(() => {
     getSubServicesHandler(slug);
   }, [slug]);
 
+  const slugToSentence = (slug) => {
+    const sentence = slug.replace(/-/g, " ");
+    return sentence.charAt(0).toUpperCase() + sentence.slice(1);
+  };
+
   return (
-    <div>
+    <>
+      <div className="pb-4 z-10 bg-base-200 sticky top-0">
+        <h1 className="text-2xl font-semibold text-content-200 capitalize">
+          {slugToSentence(slug)} Service ({subservices.length})
+        </h1>
+      </div>
       <SubserviceList />
-    </div>
+    </>
   );
 }
 
