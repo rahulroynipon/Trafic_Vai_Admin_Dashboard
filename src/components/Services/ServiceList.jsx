102584dfe7f2d2f826c6229acc51d7dcf3e4c944
rahulroynipon <rahulroynipon@gmail.com>
2025-05-29 20:43:05 +0600

    feat: enhance ServiceList and SubserviceList components; replace button navigation with Link for improved routing and add loading skeletons for subservices

--- src/components/Services/ServiceList.jsx
+++ src/components/Services/ServiceList.jsx
@@ -1,14 +1,9 @@
 import useServiceStore from "../../store/serviceStore";
-import { useNavigate } from "react-router";
+import { Link } from "react-router";
 import Skeleton from "../ui/Skeleton";
 
 function ServiceList() {
   const { services, isLoading } = useServiceStore();
-  const navigate = useNavigate();
-
-  const goToSubservice = (slug) => {
-    navigate(slug);
-  };
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-8 p-2">
@@ -19,11 +14,12 @@ function ServiceList() {
               <Skeleton key={i} className="h-60 max-w-full rounded-lg" />
             ))
         : services.map((service) => (
-            <button
+            <Link
               key={service?._id}
-              onClick={() => goToSubservice(service?.slug)}
+              to={`/services/${service?.slug}`}
               className="border border-primary rounded-lg p-5 flex flex-col items-center gap-5 
-              hover:bg-primary/10 transition-colors duration-300 cursor-pointer focus:outline-none focus-within:ring-4 focus-within:ring-primary/20"
+                hover:bg-primary/10 transition-colors duration-300 cursor-pointer 
+                focus:outline-none focus-visible:ring-4 focus-visible:ring-primary/20"
             >
               <div className="w-20 h-20">
                 <img
@@ -33,14 +29,12 @@ function ServiceList() {
                 />
               </div>
               <div className="text-center space-y-1">
-                <h2 className="font-semibold text-lg mb-1.5">
-                  {service?.name}
-                </h2>
+                <h2 className="font-semibold text-lg mb-2">{service?.name}</h2>
                 <p>Service: {service?.totalSubService} Category</p>
                 <p>Total Order: {service?.totalOrder}</p>
                 <p>Completed Order: {service?.totalCompletedOrder}</p>
               </div>
-            </button>
+            </Link>
           ))}
     </div>
   );
--- src/components/Services/SubserviceList.jsx
+++ src/components/Services/SubserviceList.jsx
@@ -1,5 +1,32 @@
+import { Link } from "react-router";
+import useServiceStore from "../../store/serviceStore";
+import Skeleton from "../ui/Skeleton";
+
 function SubserviceList() {
-  return <div></div>;
+  const { isLoading, subservices } = useServiceStore();
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-8 p-2">
+      {isLoading.getSub
+        ? Array(6)
+            .fill(0)
+            .map((_, i) => (
+              <Skeleton key={i} className="h-40 max-w-full rounded-lg" />
+            ))
+        : subservices?.map((subservice) => (
+            <div
+              key={subservice?._id}
+              className="border border-primary rounded-lg p-5 space-y-1
+                hover:bg-primary/10 transition-colors duration-300 cursor-pointer 
+                focus:outline-none focus-visible:ring-4 focus-visible:ring-primary/20"
+            >
+              <h2 className="font-semibold text-lg mb-2">{subservice?.name}</h2>
+              <p>Total Order : {subservice?.totalOrder}</p>
+              <p>Total complete order : {subservice?.totalCompletedOrder}</p>
+            </div>
+          ))}
+    </div>
+  );
 }
 
 export default SubserviceList;
